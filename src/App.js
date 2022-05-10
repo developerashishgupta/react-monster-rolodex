b4e1cf1985b76b25b8ccd7dc6afa447d5cb82a8e
Ashish Gupta <ashish.gupta03@nagarro.com>
2022-05-11 01:42:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div class="container text-center">
+      <h1> Monster Rolodex </h1>
+      <div class="row row-cols-auto">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      </div>
+      </div>
   );
 }
 
---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+function Card() {
+    return <div className="card col m-2">
+        <img src='img/$3.png' alt='Monster' className="img-thumbnail mx-auto rounded"></img>
+        <span className='name'>Leanee Graham</span>
+        <span className='mail>'>email@website.net</span>
+    </div>
+
+}
+
+export default Card;
